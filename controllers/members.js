13ac057ed5b5488407e34f6dcc280140524d148e
Jellis <venetianself@hotmail.com>
2017-08-05 18:10:47 -0500

    Commit 6 - mongo connected

--- controllers/members.js
+++ controllers/members.js
@@ -9,4 +9,8 @@ router.get('/new', (req, res)=>{
 	res.render('members/new.ejs');
 });
 
+router.post('/', (req, res)=>{
+	res.send(req.body);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const membersController = require('./controllers/members.js');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 
@@ -11,6 +14,11 @@ app.get('/', (req, res)=>{
 	res.render('index.ejs');
 });
 
+
+mongoose.connect('mongodb://localhost:27017/blog');
+mongoose.connection.once('open', ()=>{
+	console.log('connected to mongo');
+});
 
 // connection to server
 app.listen(3000, ()=>{
