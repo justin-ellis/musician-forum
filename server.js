89a500db7bcbe781b5e39b14efa78755c2b295e4
Jellis <venetianself@hotmail.com>
2017-08-05 18:01:52 -0500

    Commit 4 - Membes index page created

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const membersController = require('./controllers/members.js');
 
 
 app.get('/', (req, res)=>{
 	res.render('index.ejs');
 });
+
+app.use('/members', membersController);
+
 
 // connection to server
 app.listen(3000, ()=>{
